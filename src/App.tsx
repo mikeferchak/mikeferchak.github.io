fc5d70d8224d04b4868e6854511ae3141782fe8b
Mike Ferchak <mikeferchak@gmail.com>
2022-03-10 17:40:01 -0500

    Fix?

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Resume } from "./Resume";
 
 function App() {
   return (
-    <Router>
+    <Router basename={`/${process.env.PUBLIC_URL}`}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/resume" element={<Resume />} />
